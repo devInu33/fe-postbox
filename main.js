7c2c0bd6bcf19fcf3c42910ca42333d516aea389
Muffin <jinlog9@gmail.com>
2022-03-08 16:22:35 +0900

    refactor: main 함수 리팩토링 버튼관련 Visitor 클래스에서 처리하도록

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
 import Town from "./Town.js";
-import {Visitor} from "./Visitor.js";
+import { Visitor } from "./Visitor.js";
 import { action } from "./util.js";
 
 const init = () => {
-  const base =document.createElement('div')
+  const base = document.createElement("div");
   document.body.appendChild(base);
   base.style.height = `${document.documentElement.clientHeight}px`;
   base.style.width = `${document.documentElement.clientWidth}px`;
+
   const town = new Town(base);
+
+  const num = Math.random() * 20;
+  for (let i = 0; i < num; i++) {
+    new Town();
+  }
 
   town.render();
 
-  const visitor = new Visitor();
-  const info = document.createElement('div');
-  const button = document.createElement("button");
-  button.innerText = `빨간 우체통 확인`;
-  info.appendChild(button);
-  info.classList.add('info');
-  button.classList.add('redbutton');
-  document.body.appendChild(info);
-
-  button.addEventListener("click", () => {
-    visitor.visit(town, action);
-    visitor.print();
-  });
+  const visitor = new Visitor(town, action);
 };
 
 init();
