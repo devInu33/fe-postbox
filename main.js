62792a8625b2d60181d3f5a23381e813878ceb4d
Muffin <jinlog9@gmail.com>
2022-03-08 15:27:50 +0900

    feat: main 함수에서 town, visitor, button 생성 후 테스트 진행

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-
 import Town from "./Town.js";
+import Visitor from "./Visitor.js";
+import { action } from "./util.js";
 
-const init = ()=>{
-    const town = new Town(null);
-    town.render();
-}
+const init = () => {
+  const town = new Town(document.body);
+  town.render();
+
+  const visitor = new Visitor();
+
+  const button = document.createElement("button");
+  body.appendChild(button);
+
+  button.addEventListener("click", () => {
+    visitor.visit(town, action);
+    visitor.print();
+  });
+};
+
 init();
-
-
