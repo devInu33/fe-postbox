f30cb379611567adc3c822e87c10dc2e2f530c42
Muffin <jinlog9@gmail.com>
2022-03-08 15:27:27 +0900

    feat: Mailbox 클래스 render 함수 수정

--- Mailbox.js
+++ Mailbox.js
@@ -1,14 +1,13 @@
-export class Mailbox{
-    size;
-    el;
-    constructor(town,size) {
-        this.size=size;
-        this.town = town;
-        this.el= town.element
-    }
-    render(){
-        const box = this.el.createElement('div')
-        box.style.width =`${this.el.width/2}px`;
-        box.style.height =`${this.el.height/2}px`;
-    }
+export class Mailbox {
+  size;
+  el = document.createElement("div");
+  constructor(size) {
+    this.size = size;
+  }
+
+  render(town) {
+    this.el.style.width = `${town.width / 2}px`;
+    this.el.style.height = `${town.height / 2}px`;
+    town.element.appendChild(this.el);
+  }
 }
