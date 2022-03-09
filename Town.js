ff7b998cf8279e6c51ac0da6903c0827ff9bea29
inust33 <inust33@gmail.com>
2022-03-09 12:44:32 +0900

    Feat:added name field

--- Town.js
+++ Town.js
@@ -1,15 +1,18 @@
 import { Mailbox } from "./Mailbox.js";
 import { randomBoolean, randomNum } from "./util.js";
+import {Visitor} from "./Visitor.js";
 
 export default class Town {
   #parent;
   #children = new Set();
   mailBox = null;
   element;
+  name;
   static #objects = new Set();
 
   constructor(name = undefined, parent = null) {
     this.#parent = parent;
+    this.name = name;
     this.element = document.createElement("div");
     this.element.classList.add("town");
     this.element.dataset["name"] = name;
@@ -18,12 +21,13 @@ export default class Town {
 
   render() {
     Town.#objects.delete(this);
-    this.element.style.width = `${this.#parent.clientWidth / 2}px`;
-    this.element.style.height = `${this.#parent.clientHeight / 2}px`;
-    this.#parent.appendChild(this.element);
-
     let num = randomNum(Town.#objects.size);
     const bool = randomBoolean();
+    this.element.style.width = `${this.#parent.clientWidth / num}px`;
+    this.element.style.height = `${this.#parent.clientHeight / num}px`;
+    this.#parent.appendChild(this.element);
+
+
 
     if (bool) {
       this.createMailbox();
@@ -39,8 +43,9 @@ export default class Town {
     }
   }
   createMailbox() {
-    const size = Math.random() + 1;
+    const size = Math.floor(Math.random()*Town.#objects.size+1);
     this.mailBox = new Mailbox(size, this.element);
+    Visitor.boxes.add(this)
     this.mailBox.render();
   }
 }
