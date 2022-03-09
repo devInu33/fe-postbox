9b04fde81724748a9e232f8371b2e0f0fbac3cb3
inust33 <inust33@gmail.com>
2022-03-09 12:45:04 +0900

    refactor:action function

--- util.js
+++ util.js
@@ -2,21 +2,22 @@ import { Visitor } from "./Visitor.js";
 import { Mailbox } from "./Mailbox.js";
 
 export const randomBoolean = () => Math.random() < 0.5;
-export const quickSort = (array, left = 0, right = array.length - 1) => {
+export const quickSort = (array, compare, left = 0, right = array.length - 1  ) => {
   if (left >= right) {
     return;
   }
+
   const mid = Math.floor((left + right) / 2);
   const pivot = array[mid];
   const partition = divide(array, left, right, pivot);
-  quickSort(array, left, partition - 1);
-  quickSort(array, partition, right);
+  quickSort(array, compare, left, partition - 1);
+  quickSort(array, compare, partition, right);
   function divide(array, left, right, pivot) {
-    while (left <= right) {
-      while (array[left] < pivot) {
+    while (left<= right) {
+      while (compare(array[left], pivot)) {
         left++;
       }
-      while (array[right] > pivot) {
+      while (compare(array[right],pivot)) {
         right--;
       }
       if (left <= right) {
