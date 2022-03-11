fb22c1c939b31c9921e9471e181afa826fc876c7
inust33 <inust33@gmail.com>
2022-03-11 15:22:58 +0900

    fix:conflict

---
+++ Town.js
@@ -0,0 +1,36 @@
+
+import { Model } from "./Model.js";
+import { randomBoolean, randomNum } from "./util.js";
+import { Mailbox } from "./Mailbox.js";
+
+
+export default class Town extends Model {
+  mailBox = null;
+  name;
+
+  // #next = null;
+  constructor(name = undefined, parent = null) {
+    super(parent);
+    this.name = name;
+  }
+
+  *_render() {
+    let num = randomNum(Model.objects.size);
+    parent ? this.el.classList.add("town") : this.el.classList.add("base");
+    this.el.dataset["name"] = this.name;
+    if (randomBoolean()) {
+      new Mailbox(randomNum(num), this);
+    }
+    if (!Model.objects.size) return;
+    for (const model of Model.objects){
+      if(num<=0)break;
+      num-=1;
+      model.parent = this;
+      this.children.add(model)
+      yield model;
+    }
+
+
+  }
+
+}
---
+++ util.js
@@ -0,0 +1,45 @@
+
+import { DomVisitor } from "./Visitor.js";
+
+
+export const randomBoolean = () => Math.random() < 0.5;
+export const quickSort = (
+  array,
+  compare,
+  left = 0,
+  right = array.length - 1
+) => {
+  if (left >= right) {
+    return;
+  }
+
+  const mid = Math.floor((left + right) / 2);
+  const pivot = array[mid];
+  const partition = divide(array, left, right, pivot);
+  quickSort(array, compare, left, partition - 1);
+  quickSort(array, compare, partition, right);
+  function divide(array, left, right, pivot) {
+    while (left <= right) {
+      while (compare(array[left], pivot)) {
+        left++;
+      }
+      while (compare(array[right], pivot)) {
+        right--;
+      }
+      if (left <= right) {
+        let swap = array[left];
+        array[left] = array[right];
+        array[right] = swap;
+        left++;
+        right--;
+      }
+    }
+    return left;
+  }
+  return array;
+};
+
+export const randomNum = (num) => Math.floor(Math.random() * num) + 1;
+
+
+export const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
